c37f4e92fd0fd98041dc2f4314e2149819768a18
Patrick <patrickcabrera2025@gmail.com>
2026-04-01 18:38:26 -0500

    avance main

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
 from src.db import crear_tablas
 
+
 crear_tablas()
+
+#Comienzo del menú
+while True :
+    print("\nBIENVENIDO A BUDGET TRACKER\n")
+    print("1.Agregar gasto")
+    try :
+        opcion = int(input("Selecciona una opción :"))
+        if opcion <= 0 or opcion > 5 :
+            print("Opción inválida")
+            
+    except Exception as e :
+        print(f"Error : {e}")
+        
+
 
